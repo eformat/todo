369535cdd9a572029c91d5ba6ed93a057aa49933
Phillip Kruger <phillip.kruger@gmail.com>
2023-03-16 09:57:32 +1100

    real start
    Signed-off-by: Phillip Kruger <phillip.kruger@gmail.com>

--- src/test/java/au/com/brisbane/GreetingResourceTest.java
+++ src/test/java/au/com/brisbane/GreetingResourceTest.java
@@ -12,10 +12,10 @@ public class GreetingResourceTest {
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/hello/Brisbane")
+          .when().get("/hello")
           .then()
              .statusCode(200)
-             .body(is("Hello Brisbane"));
+             .body(is("Hello world"));
     }
 
 }
